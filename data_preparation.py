7849ddbf1567e1467fd42c26ab89c97b479ef0bd
ecem4 <147860748+ecem4@users.noreply.github.com>
2025-01-24 16:20:07 +0100

    Update data_preparation.py
    Initial info about function_2 : check_assumptions

--- data_preparation.py
+++ data_preparation.py
@@ -1,1 +1,6 @@
-nano data_preparation.py
+#Function nr 2
+#Dependent variable: "STAI_Trait_Anxiety" 
+#Predictors:"CERQ_SelfBlame", "CERQ_Rumination", "CERQ_Catastrophizing", "COPE_SelfBlame", "LOT_Optimism", "LOT_Pessimism", "PSQ_Worries", "PSQ_Tension"
+#The Function: 
+#Input: Output of function nr 1
+#Output: Checking how strong the correlation between predictors and dependent variable, making boxplots to find and remove outliers, and homogeneity of variance.
